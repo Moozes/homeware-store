c2414f5d8097d718419d058faeaa34f33987eeb4
Khodja Moussa <khodja.moussa.pro@gmail.com>
2023-06-19 18:53:10 +0100

    Create Stepper componenet

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import Button from "components/Button";
 import TextInput from "components/TextInput";
 import { colors } from "utils/themes";
+import Stepper from "components/Stepper";
 
 
 function App() {
   return (
     <div style={{background: colors.primary}} >
-      starter template
-      <TextInput placeholder="your@email.com" variant="opaque" />
+      <Stepper/>
     </div>
   );
 }
